71c1f396722048c24293e391d370816f8a7460b4
jjshoots <taijunjet@hotmail.com>
2021-01-07 23:43:26 +0000

    added instagram notif

--- scripts/functions.js
+++ scripts/functions.js
@@ -9,7 +9,7 @@ function renameTitle(){
 function wipeNotifications(){
     // get rid of notifications
     var NotifObject = document.getElementsByClassName("s45kfl79 emlxlaya bkmhp75w spb7xbtv pq6dq46d e9vueds3 ekzkrbhg omvj5yrc jiuqdcnw d82f96u3 gky8063y");
-    
+
     if(NotifObject.length > 0){
         NotifObject[0].remove();
     }
@@ -18,7 +18,7 @@ function wipeNotifications(){
 function wipeOthers(){
     // get rid of notifications
     var NotifObject = document.getElementsByClassName("sibfvsnu px9q9ucb j2ut9x2k p4hiznlx pq6dq46d e9vueds3 ekzkrbhg omvj5yrc jiuqdcnw d82f96u3 gky8063y");
-    
+
     if(NotifObject.length > 0){
         NotifObject[0].remove();
     }
